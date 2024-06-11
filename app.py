933765a833d42b521a5240f5a442ac49ab2fd370
QuakeScan <quakescan@outlook.com>
2024-06-11 17:05:30 +0200

    update code

--- app.py
+++ app.py
@@ -10,31 +10,10 @@ import tensorflow as tf
 app = Flask(__name__)
 
 longitud, altura = 150, 150
-modelo = 'modelo.h5'
-pesos = 'pesos.h5'
+modelo = 'Url del modelo'
+pesos = 'url del modelo'
 
 
-
-class YourCustomAdamOptimizer(tf.keras.optimizers.Optimizer):
-    def __init__(self, learning_rate=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-7, **kwargs):
-        super(YourCustomAdamOptimizer, self).__init__(**kwargs)
-        self.learning_rate = learning_rate
-        self.beta_1 = beta_1
-        self.beta_2 = beta_2
-        self.epsilon = epsilon
-
-    def get_updates(self, loss, params):
-        grads = self.get_gradients(loss, params)
-        self.iterations = self.iterations + 1
-        lr = self.learning_rate * tf.math.sqrt(1 - tf.math.pow(self.beta_2, self.iterations)) / (1 - tf.math.pow(self.beta_1, self.iterations))
-        ms = [tf.Variable(tf.zeros_like(p), trainable=False) for p in params]
-        vs = [tf.Variable(tf.zeros_like(p), trainable=False) for p in params]
-        self.weights = [self.iterations] + ms + vs
-
-        updates = [tf.compat.v1.assign(m, self.beta_1 * m + (1 - self.beta_1) * g) for m, g in zip(ms, grads)]
-        updates += [tf.compat.v1.assign(v, self.beta_2 * v + (1 - self.beta_2) * tf.math.square(g)) for v, g in zip(vs, grads)]
-        updates += [tf.compat.v1.assign_sub(p, lr * m / (tf.math.sqrt(v) + self.epsilon)) for p, m, v in zip(params, ms, vs)]
-        return updates
 
 cnn = load_model(modelo, compile=False)
 cnn.load_weights(pesos)
@@ -54,7 +33,8 @@ def Predict():
     x = np.expand_dims(resizedImage, axis=0)   
     predictions = cnn.predict(x)  
     predicted_class = np.argmax(predictions, axis=1) 
-    class_names = ['COLLAPSE', 'HEALTHY', 'PARTIAL']
+    class_names = ['COLLAPSE', 'COMBINED CORNERS', 'DAMAGE TO INFILL', 'DISLODGEMENTS', 'IP PARTIAL COLLAPSE', 'PARTIAL COLLAPSE', 'POUNDING LEANING',
+                   'RC ST FAILURE', 'SPALLING', 'TOE CRUSHING']
     predicted_label = class_names[predicted_class[0]]
     res = jsonify(result=predicted_label, status = True)
     return make_response(res, 200)
